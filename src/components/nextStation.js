d158853b98cb1575763d5913f4efda82876291bf
newnorawich <newsnora@gmail.com>
2022-11-29 22:56:03 +0700

    Edit Condition and Add Use Strict

--- src/components/nextStation.js
+++ src/components/nextStation.js
@@ -1,3 +1,4 @@
+"use strict";
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Dimensions, } from 'react-native';
 
@@ -5,31 +6,32 @@ const screenHeight = Dimensions.get('window').height;
 const start = 1;
 
 const NextStation = (props) => {
-    if(props.navigate)
-    {
-        return (
-            <View style = {[Styles.container, {backgroundColor: 'rgba(0,0,0,0.7)'}]}>
-                <View style = {Styles.next_station_view}>
-                    <Text style = {Styles.next_station_text}>
-                        {start==0? "Nearest": "Next"}
-                    </Text>
-                    <Text style = {Styles.next_station_text}>
-                        Station
-                    </Text>
-                </View>
-                <View style = {Styles.station_name_view}>
-                    <Text style = {Styles.station_name_text}>
-                        {props.stationName}
-                    </Text>
-                    <View style = {[Styles.station_route_view, { backgroundColor: props.stationColor }]}>
-                        <Text style = {Styles.station_route_text}>
-                            {props.stationPlatform}
+    return (
+        <View>
+            {props.navigate && (
+                <View style = {[Styles.container, {backgroundColor: 'rgba(0,0,0,0.7)'}]}>
+                    <View style = {Styles.next_station_view}>
+                        <Text style = {Styles.next_station_text}>
+                            {start==0? "Nearest": "Next"}
+                        </Text>
+                        <Text style = {Styles.next_station_text}>
+                            Station
                         </Text>
                     </View>
-                </View>
-            </View>
-        );
-    }
+                    <View style = {Styles.station_name_view}>
+                        <Text style = {Styles.station_name_text}>
+                            {props.stationName}
+                        </Text>
+                        <View style = {[Styles.station_route_view, { backgroundColor: props.stationColor }]}>
+                            <Text style = {Styles.station_route_text}>
+                                {props.stationPlatform}
+                            </Text>
+                        </View>
+                    </View>
+                </View> 
+            )}
+        </View>
+    );
 }
 
 const Styles = StyleSheet.create({
