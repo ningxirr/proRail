e5e6dfef83d0ef7c8e2c96cf2b00f2d3672f4b6c
newnorawich <newsnora@gmail.com>
2022-12-20 01:15:27 +0700

    add star icon with new fonts

--- src/components/Result/Body/Choices.js
+++ src/components/Result/Body/Choices.js
@@ -1,6 +1,7 @@
 "use strict";
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions, ScrollView, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const screenHeight = Dimensions.get('window').height;
 const screenWidth = Dimensions.get('window').width;
@@ -8,8 +9,15 @@ const screenWidth = Dimensions.get('window').width;
 const Choices = (props) => {
     return(
       <TouchableOpacity style={Styles.choice_component_view} onPress={props.function}>
-        <Text style={Styles.number_text}>{props.number}</Text>
-        <Text style={Styles.unit_text}>{props.unit}</Text>
+        <View style={Styles.icon_and_number_view}>
+          <View>
+            {props.recommended ? props.selected?<Icon name='star' color={'#000'} width={screenWidth*0.05} size={20}/>:<Icon name='star-o' color={'#000'} width={screenWidth*0.05} size={20}/>:null}
+          </View>
+          <View>
+            <Text style={Styles.number_text}>{props.number}</Text>
+            <Text style={Styles.unit_text}>{props.unit}</Text>
+          </View>
+        </View>
         <View style={{flex: 1, justifyContent: 'flex-end'}}>
           <Text style={[Styles.topic_text,{color:props.selected?'black':'#818181'}]}> 
             {props.choice}
@@ -26,23 +34,29 @@ const Styles = StyleSheet.create({
       height: screenHeight*0.17,
       marginHorizontal:screenWidth*0.01, 
       marginTop:screenHeight*0.02,
-      backgroundColor: '#cdcdcd',
+      backgroundColor: '#D7D6D6',
       borderRadius:10, 
       padding: screenHeight*0.015
+  },
+  icon_and_number_view:{
+      flexDirection: 'row',
+      justifyContent: 'space-between',
   },
   number_text: {
       fontSize:screenHeight*0.042, 
       color:'black', 
-      fontWeight:'500', 
+      fontFamily: 'LINESeedSans_A_Bd',
       textAlign:'right'
   },
   unit_text: {
     fontSize:screenHeight*0.018, 
+    fontFamily: 'LINESeedSans_A_Rg',
     color:'black', 
     textAlign:'right'
   },
   topic_text: {
     fontSize:screenHeight*0.018, 
+    fontFamily: 'LINESeedSans_A_Rg',
     marginBottom:screenHeight*0.01, 
     textAlign:'left'
   }
