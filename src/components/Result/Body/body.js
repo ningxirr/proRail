bcda059c96fe2c4f93c214ba33c5899b362e8f02
newnorawich <newsnora@gmail.com>
2022-12-20 01:13:40 +0700

    Implement new ui

--- src/components/Result/Body/body.js
+++ src/components/Result/Body/body.js
@@ -4,91 +4,132 @@ import { StyleSheet, Text, View, Dimensions, ScrollView } from 'react-native';
 import Route from './route';
 import RecommendedRoute from './recommendedRoute';
 import Choices from './Choices';
+import CustomButton from '../../customButton';
 
 const screenHeight = Dimensions.get('window').height;
 const screenWidth = Dimensions.get('window').width;
 
 const Body = (props) => {
-  const [path, setpath] = useState('Fastest');
+  const [path, setPath] = useState('Fastest');
+  const [choice, setMoreChoice] = useState(true);
   return(
     <View>
       {/* RecommendedRoute */}
-      <View style ={{ marginTop: -screenHeight*0.1 }}>
+      <View style ={[{ marginTop: -screenHeight*0.08 }, Styles.recommended_route_with_summary_view]}>
         <View style={Styles.recommended_route_view}>
-          <RecommendedRoute topic = {'Times'} number = {'36'} unit = {'mins'} />
-          <RecommendedRoute topic = {'Interchange'} number = {'1'} unit = {'station(s)'}/>
-          <RecommendedRoute topic = {'Price'} number = {'65'} unit = {'baht'}/>
+          <RecommendedRoute topic = {'TIME'} number = {'18'} unit = {'mins'} />
+          <RecommendedRoute topic = {'INTERCHANGE'} number = {'1'} unit = {'station(s)'}/>
+          <RecommendedRoute topic = {'PRICE'} number = {'24'} unit = {'THB'}/>
+        </View>
+        <View style={Styles.all_route_summary_view}>
+          <View style={[{width:'50%'}, {borderRightWidth: 1}]}>
+            <Text style ={Styles.summary_text}>
+              Travel time: 36 mins
+            </Text>
+          </View>
+          <View style={{width:'50%'}}>
+            <Text style ={Styles.summary_text}>
+              Total price: 48 THB
+            </Text>
+          </View>
         </View>
       </View>
 
-      <View style = {Styles.body_view}>
-        <Text style = {Styles.route_text}>
-          Route
-        </Text>
+      <View style = {Styles.select_choice_view}>
+        <CustomButton   text={'Recommended'}
+                        backgroundColor={choice? 'black': 'white'} 
+                        borderColor={choice? 'white': 'black'} 
+                        textColor={choice? 'white': 'black'} 
+                        width = {'49%'}
+                        function={()=> {
+                          props.changeHeader('Fastest')
+                          setMoreChoice(true)
+                          setPath('Fastest')
+                        }}/>
+        <CustomButton  text={'More Choices'} 
+                        backgroundColor={choice? 'white': 'black'} 
+                        borderColor={choice? 'black': 'white'} 
+                        textColor={choice? 'black': 'white'} 
+                        width = {'49%'}
+                        function={()=> {
+                          props.changeHeader('Cheapest')
+                          setMoreChoice(false)
+                          setPath('Cheapest')
+                        }}/>
       </View>
-
-      <Route moreDetail={true} path={path}/>
 
       {/* MoreChoice */}
-      <View style={Styles.more_choice_component_view}>
-        <Text style={Styles.more_choice_text}>
-          More Choices
-        </Text>
-        <ScrollView horizontal={true} showsVerticalScrollIndicator={false}>
-          <View style={{margin:screenWidth*0.02}}/>
-          <Choices number={28} unit={'mins'} choice={'Fastest'} selected={path==='Fastest'} function={()=>{
-              props.changeHeader('Fastest')
-              setpath('Fastest')
-            }}/>
-          <Choices number={35} unit={'baht'} choice={'Cheapest'} selected={path==='Cheapest'} function={()=> {
-              props.changeHeader('Cheapest')
-              setpath('Cheapest')
-            }}/>
-          <Choices number={1} unit={'station(s)'} choice={'Least\nInterchanges'} selected={path==='Least Interchanges'} function={()=> {
-              props.changeHeader('Least Interchanges')
-              setpath('Least Interchanges')
-            }}/>
-          <View style={{margin:screenWidth*0.02}}/>
-        </ScrollView>
-      </View>
+      {
+        choice? null:
+        <View style={Styles.more_choice_component_view}>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+            <View style={{margin:screenWidth*0.02}}/>
+            <Choices number={35} unit={'baht'} choice={'Cheapest'} selected={path==='Cheapest'} recommended={false} function={()=> {
+                props.changeHeader('Cheapest')
+                setPath('Cheapest')
+              }}/>
+            <Choices number={1} unit={'station(s)'} choice={'Least\nInterchanges'} recommended={false} selected={path==='Least Interchanges'} function={()=> {
+                props.changeHeader('Least Interchanges')
+                setPath('Least Interchanges')
+              }}/>
+            <View style={{margin:screenWidth*0.02}}/>
+          </ScrollView>
+        </View>
+      }
 
+      <View>
+        <Route moreDetail={true} path={path}/>
+      </View>
     </View>
     
   );
 };
 
 const Styles = StyleSheet.create({
-  body_view: {
-    width: '100%',
-    paddingTop: '3%',
+  select_choice_view: {
+    paddingVertical: '2%',
     paddingHorizontal: '5%',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  recommended_route_with_summary_view:{
+    width: '90%',
+    marginHorizontal: screenWidth*0.05,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 10,
   },
   recommended_route_view: {
-    width: '90%',
-    marginHorizontal: '5%',
     flexDirection: 'row',
     backgroundColor: 'black',
-    paddingHorizontal: '7%', 
+    paddingHorizontal: '10%', 
     paddingVertical: screenHeight*0.019,
     borderRadius: 10,
     justifyContent: 'space-between',
   },
+  all_route_summary_view:{
+    padding: '2.5%',
+    flexDirection: 'row',
+  },
+  summary_text:{
+    fontSize: screenHeight*0.016,
+    fontFamily: 'LINESeedSans_A_Rg',
+    textAlign: 'center',
+  },
   route_text: {
     color:'black', 
     fontSize: screenHeight*0.025, 
-    fontWeight:'500'
+    fontFamily: 'LINESeedSans_A_Bd'
   },
   more_choice_component_view: {
     width: screenWidth,
-    marginTop: screenHeight*0.03,
-    marginBottom: screenHeight*0.05,
-    borderTopColor:'#E4E4E4',
-    borderTopWidth:1,
+    marginBottom: screenHeight*0.02
   },
   more_choice_text: {
     fontSize:screenHeight*0.036, 
     color:'black', 
-    fontWeight:'bold', 
+    fontFamily: 'LINESeedSans_A_Bd',
+    paddingTop: '5%',
     paddingHorizontal: '5%'
   },
 });
