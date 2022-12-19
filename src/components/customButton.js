868a04a9ac64fd87f4069b2e90c2bf8291b20027
newnorawich <newsnora@gmail.com>
2022-12-20 01:13:12 +0700

    update custom button for more flexibility

--- src/components/customButton.js
+++ src/components/customButton.js
@@ -7,7 +7,7 @@ const screenWidth = Dimensions.get('window').width;
 
 const CustomButton = (props) => {
     return(
-        <TouchableOpacity style = {[Styles.button, {borderColor: props.borderColor, backgroundColor: props.backgroundColor}]} onPress={props.function}>
+        <TouchableOpacity style = {[Styles.button, {borderColor: props.borderColor, backgroundColor: props.backgroundColor, width: props.width}]} onPress={props.function}>
             <Text style = {[Styles.button_text, {color: props.textColor}]}>
                 {props.text}
             </Text>
@@ -17,9 +17,8 @@ const CustomButton = (props) => {
 
 const Styles = StyleSheet.create({  
     button: {
-        width: '100%',
         height: screenHeight*0.05,
-        alignSelf: "center", 
+        alignSelf: 'center', 
         borderColor:'black', 
         borderWidth:1,
         marginVertical:'1%',
@@ -29,7 +28,8 @@ const Styles = StyleSheet.create({
     button_text: {
         textAlign:'center', 
         alignContent:'center', 
-        fontSize: screenHeight*0.017
+        fontSize: screenHeight*0.017,
+        fontFamily: 'LINESeedSans_A_Rg',
     }
 });
 
