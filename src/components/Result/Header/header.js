28f26ac56ec3c5421694b338b4974bd4428b8f45
newnorawich <newsnora@gmail.com>
2022-12-20 01:14:38 +0700

    implement stop count and new font

--- src/components/Result/Header/header.js
+++ src/components/Result/Header/header.js
@@ -12,25 +12,29 @@ const navigate = false;
 const Header = (props) => {
     return (
       <View>
-        <ImageBackground  source={require('../../../../assets/images/background.png')}>
-          <View style = {{backgroundColor: 'rgba(0,0,0,0.7)'}}>
+        <ImageBackground source={require('../../../../assets/images/background.png')} style={{height:screenHeight*0.35}}> 
             <NavBar /> 
             <View style = {navigate ? Styles.header_view: Styles.header_no_navigate_view}>
               <View style = {Styles.header_body_view}>
-                <NextStation stationName = {'Bang Wa'} stationPlatform = {'MRT'} stationColor = {'#325E9A'} backgroundColor={'#595959'} navigate={navigate}/>
-                <Text style = {{color:'white', fontSize: screenHeight * 0.038, fontWeight:'bold'}}>
-                  {props.header}
-                </Text>
+                <View style = {Styles.main_header_view}>
+                  <Text style = {[Styles.header_text, {fontSize: props.header.length > 10 ? screenHeight*0.028: screenHeight*0.038}]}>
+                    {props.header}
+                  </Text>
+                  <View style={Styles.add_stop_view}>
+                    <Text style={Styles.add_stop_text}>
+                      2 stop(s)
+                    </Text>
+                  </View>
+                </View>
                 <View style = {Styles.start_and_end_route_view}>
-                  <StartAndEndRoute stationName = {'Kasetsart University'} stationPlatform = {'BTS skytrain'} stationColor = {'#76B729'}/>
-                  <Text style = {{color:'white', fontSize: screenHeight * 0.020, fontWeight:'normal'}}>
+                  <StartAndEndRoute stationName = {'Kasetsart University'} stationPlatform = {'BTS skytrain'} stationColor = {'#4CAF1D'}/>
+                  <Text style = {{color:'white', fontSize: screenHeight * 0.020, fontFamily: 'LINESeedSans_A_Rg'}}>
                     to
                   </Text>
-                  <StartAndEndRoute stationName = {'Phahonyothin'} stationPlatform = {'MRT'} stationColor = {'#325E9A'}/>
+                  <StartAndEndRoute stationName = {'Huai Khwang'} stationPlatform = {'MRT'} stationColor = {'#325E9A'}/>
                 </View>
               </View>
             </View>
-          </View>
         </ImageBackground>
       </View>
     );
@@ -43,6 +47,10 @@ const Header = (props) => {
       paddingHorizontal: screenWidth*0.005,
       paddingVertical: screenHeight*0.03,
     },
+    header_text: {
+      color:'white', 
+      fontFamily: 'LINESeedSans_A_Bd'
+    },
     header_no_navigate_view: {
       width: screenWidth,
       height: screenHeight*0.35,
@@ -50,7 +58,26 @@ const Header = (props) => {
       paddingVertical: screenHeight*0.01,
     },
     header_body_view:{
-      paddingHorizontal: '5%',
+      paddingHorizontal: '5%'
+    },
+    main_header_view: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    add_stop_view:{
+      borderColor: 'white',
+      borderWidth: 1,
+      borderRadius: 100,
+      justifyContent: 'center'
+    },
+    add_stop_text: {
+      color:'white',
+      fontSize: screenHeight * 0.016,
+      fontFamily: 'LINESeedSans_A_Bd',
+      textAlign: 'center',
+      alignContent:'center', 
+      paddingVertical: '2%',
+      paddingHorizontal: '4%'
     },
     start_and_end_route_view: {
       width: '100%',
