ecbea054d011c4bee00fb1555e4c567762bd6c44
newnorawich <newsnora@gmail.com>
2023-01-17 00:16:18 +0700

    add navigate screen

--- src/views/Navigate.js
+++ src/views/Navigate.js
@@ -1,86 +1,56 @@
 "use strict";
-import React, { useCallback, useMemo, useRef, useState } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, View, Button, Dimensions, Image } from 'react-native';
-import BottomSheet, {BottomSheetScrollView, BottomSheetHandle} from '@gorhom/bottom-sheet';
-import GestureRecognizer, {swipeDirections, } from 'react-native-swipe-gestures';
+
+import React, { useCallback, useMemo, useRef } from 'react';
+import { SafeAreaView, StyleSheet, Text, View,  Dimensions, ImageBackground } from 'react-native';
+import BottomSheet, { BottomSheetScrollView } from '@gorhom/bottom-sheet';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import NextStation from '../components/nextStation';
 import AllRoute from '../components/allRoute';
+import TochableIcon from '../components/tochableIcon';
 
 const screenHeight = Dimensions.get('window').height;
 const screenWidth = Dimensions.get('window').width;
 
-const Result = () => {
-  // hooks
+const Result = (props) => {
   const sheetRef = useRef(null);
-  const snapPoints = useMemo(() => ["25%", "55%", "90%"], []);
-  const [snapIndex, setSnapIndex] = useState(1);
+  const snapPoints = useMemo(() => ["55%", "100%"], []);
 
-  // callbacks
   const handleSheetChange = useCallback((index) => {
     console.log("handleSheetChange", index);
-    setSnapIndex(index);
   }, []);
-  const handleSnapPress = useCallback((index) => {
-    sheetRef.current?.snapToIndex(index);
-  }, []);
-
-  const config = {
-    velocityThreshold: 0.3,
-    directionalOffsetThreshold: 80
-  };
 
   return (
     <SafeAreaView style={Styles.container}>
+      <ImageBackground source={require('../../assets/images/Map.png')} resizemode='contain' style={{height:screenHeight*0.7}} >
         <View style={Styles.header_view}>
-          <Text style={Styles.header_text}>
-            Navigate
-          </Text>
-          <Icon name='close' color={'white'} width={screenWidth*0.05} size={20}/>
+        <Text style={Styles.header_text}>
+          Navigate
+        </Text>
+        <TochableIcon name={'close'} color={'white'} width={screenWidth*0.05} size={20} function={()=> props.navigation.navigate('FavoriteRoute')}/>
         </View>
         <View style={Styles.navigation_view}>
           <NextStation navigate={true} stationName={'Keha'} stationColor={'#77CC00'} stationPlatform={'BTS skytrain'}/>
         </View>
-        {/* <Image source={require('../../assets/images/Map.png')} style={{ width: '100%', height: '50%'}} resizeMode={'repeat'}/> */}
-
-        <GestureHandlerRootView style={{ flex: 1 }}>
+      </ImageBackground>
+      <Text style={{textAlign:'center', fontFamily: 'LINESeedSans_A_Bd',}}>IG: new_norawich</Text>
+        <GestureHandlerRootView style={{ flex: 1, marginTop: '-100%' }}>
           <BottomSheet 
             ref={sheetRef} 
-            index={1} 
+            index={0} 
             snapPoints={snapPoints} 
             onChange={handleSheetChange} 
             overDragResistanceFactor={10}
-            handleComponent={() => {
-              switch(snapIndex){
-                case 0:
-                  return (<Icon name='chevron-up' color={'#5A5A5A'} width={screenWidth*0.05} size={20} style={{alignSelf:'center'}}/>)
-                case 1:
-                  return (<Icon name='minus' color={'#5A5A5A'} width={screenWidth*0.05} size={20} style={{alignSelf:'center'}}/>)
-                case 2:
-                  return (<Icon name='chevron-down' color={'#5A5A5A'} width={screenWidth*0.05} size={20} style={{alignSelf:'center'}}/>)
-              }
-            }}
-            // handleComponent = {() => {return (<Icon name='angle-up' color={'black'} width={screenWidth*0.05} size={20}/>)}}
-            style={{
-              backgroundColor: 'white',  // <==== HERE
-              borderRadius: 24,
-              shadowColor: '#000000',
-              shadowOffset: {
-                width: 0,
-                height: 8,
-              },
-              shadowOpacity: 0.1,
-              shadowRadius: 24,
-              elevation: 10,
-            }}>
-            <BottomSheetScrollView contentContainerStyle={Styles.contentContainer}>
-              <AllRoute moreDetail={true} path={'Fastest'}/>
+            handleComponent={() => <></>}
+            style={Styles.bottom_sheet}
+            // enableOverDrag={false}
+            >
+            <BottomSheetScrollView contentContainerStyle={Styles.content_bottom_sheet_scroll_view}>
+              <AllRoute moreDetail={true} path={props.route.params.path} routes={props.route.params.routes}/>
+              {console.log(props.route.params.path)}
             </BottomSheetScrollView>
           </BottomSheet> 
         </GestureHandlerRootView>
-        
-      {/* </GestureRecognizer> */}
     </SafeAreaView>
     
   );
@@ -111,16 +81,25 @@ const Styles = StyleSheet.create({
   header_text:{
     color: 'white',
     fontSize: screenHeight*0.03,
+    fontFamily: 'LINESeedSans_A_Bd',
   },
-  contentContainer: {
+  content_bottom_sheet_scroll_view: {
     backgroundColor: "white",
+    paddingTop: '2%',
     paddingBottom: '5%'
   },
-  itemContainer: {
-    padding: 6,
-    margin: 6,
-    backgroundColor: "#eee",
-  },
+  bottom_sheet: {
+    borderRadius: 20,
+    overflow: 'hidden',
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 8,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 24,
+    elevation: 10,
+  }
 });
 
 
