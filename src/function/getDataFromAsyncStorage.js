372231e0c013a718b87b178426fec50824166fa0
newnorawich <newsnora@gmail.com>
2023-03-11 23:19:52 +0700

    add async storage method

---
+++ src/function/getDataFromAsyncStorage.js
@@ -0,0 +1,11 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+module.exports = async function (key) {
+    try {
+      const jsonValue = await AsyncStorage.getItem(key);
+      console.log("GetRromAsyncStorage: "+ jsonValue)
+      return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch(e) {
+      console.error('Error getting recommended data:', e);
+    }
+  }
