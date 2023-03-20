7bff821ffc8854c7a2b81f6fd55a585da7b42d82
newnorawich <newsnora@gmail.com>
2023-03-20 23:56:02 +0700

    adding notification handler

--- index.js
+++ index.js
@@ -3,7 +3,17 @@
  */
 
 import {AppRegistry} from 'react-native';
+import notifee, { EventType } from '@notifee/react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+notifee.onBackgroundEvent(async ({ type, detail }) => {
+    const { notification, pressAction } = detail;
+  
+    // Check if the user pressed the "Mark as read" action
+    if (type === EventType.ACTION_PRESS && pressAction.id === 'mark-as-read') {
+      await notifee.cancelNotification(notification.id);
+    }
+  });
 
 AppRegistry.registerComponent(appName, () => App);
