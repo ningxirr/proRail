ef451aec40c10d648c1be328d64be2b1618fc8d9
newnorawich <newsnora@gmail.com>
2023-04-11 00:27:56 +0700

    edit space

--- src/components/RailMap.js
+++ src/components/RailMap.js
@@ -12,7 +12,7 @@ const destMarkPic = require('../../assets/images/oridestMark.png');
 const stopMarkPic = require('../../assets/images/stopMark.png');
 const {width} = Dimensions.get('window');
 
-const RailMap = ({oriStationCode, destStationCode, itemsCode, cannotClicked, num, notSelectedStation, fromAddStop, setSelectedType,canAddStop,selectedPostition, setSelectedCodeAddStop}) => {
+const RailMap = ({oriStationCode, destStationCode, itemsCode, cannotClicked, num, notSelectedStation, fromAddStop, setSelectedType, canAddStop, selectedPostition, setSelectedCodeAddStop}) => {
   const ratioTransform = 3.30527928;
   const [scale, setScale] = useState('');
   const [modalVisible, setModalVisible] = useState(false);
