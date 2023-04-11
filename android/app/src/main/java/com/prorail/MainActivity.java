78d6d39b50767174c2525341e99fd5922caa282f
ningxirr <85488465+ningxirr@users.noreply.github.com>
2023-04-12 01:16:04 +0700

    delete config splash for android

--- android/app/src/main/java/com/prorail/MainActivity.java
+++ android/app/src/main/java/com/prorail/MainActivity.java
@@ -1,10 +1,8 @@
 package com.prorail;
 
-import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -16,12 +14,6 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "ProRail";
   }
-
-  @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this);  // here
-        super.onCreate(savedInstanceState);
-    }
 
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
